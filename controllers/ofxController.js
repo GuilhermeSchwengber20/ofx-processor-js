68f015bcb092d40311acf561993a02a58005f6d8
GuilhermeSchwengber20 <guilhermenft2021@gmail.com>
2024-04-23 13:55:51 -0300

    adicionando logs para saber por onde esta passando o processamento de ofx

--- controllers/ofxController.js
+++ controllers/ofxController.js
@@ -11,18 +11,22 @@ class OfxController {
                 error: "Arquivo OFX não fornecido. Certifique-se de incluir o arquivo OFX no corpo da solicitação.",
             };
         }
+        console.log("-----------------")
         const base64Data = ofxData;
-
+        console.log("Recebeu ofxData")
+        console.log("-----------------")
         const ofxString = Buffer.from(base64Data, "base64").toString("utf8");
 
         const parsedData = ofx.parse(ofxString);
-
+        console.log("Ofx processado!")
+        console.log("-----------------")
         const transactions = parsedData.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN.map(
             (ofxTransaction) => {
                 return OfxModel.parseTransaction(ofxTransaction);
             }
         );
 
+        console.log("Retornou transações")
         return transactions;
     }
 }
--- routes/ofxRoutes.js
+++ routes/ofxRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 
 router.post("/process-ofx", (req, res) => {
+    console.log("bateu na rota /process-ofx")
     const ofxData = req.body.ofxData;
     
     const transactions = OfxController.processOfx(ofxData);
